7e6c933b9d33a1fc95696235dd99983edad1c92b
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-08-23 18:34:47 -0400

    Fixed substring

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,6 +1,6 @@
 var site = window.location;
 if (site.search.length > 1) {
     var newSite = site.origin;
-    newSite += site.search.substring(1);
+    newSite += site.search.substring(0);
     site.replace(newSite);
 }
