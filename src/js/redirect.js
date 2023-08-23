1507e0128d3a96c0011c605641497e3109ed08a3
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-08-23 18:36:49 -0400

    Fixed substring again.

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,6 +1,6 @@
 var site = window.location;
 if (site.search.length > 1) {
     var newSite = site.origin;
-    newSite += site.search.substring(0);
+    newSite += site.search.substring(1);
     site.replace(newSite);
 }
