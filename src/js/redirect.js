9586c9ee106a72911d5cf91c5e5a30f2981f6902
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-08-23 19:00:52 -0400

    Reorder function pathing

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,6 +1,8 @@
-var site = window.location;
-if (site.search.length > 1) {
-    var newSite = site.origin;
-    newSite += site.search.substring(1);
-    site.replace(newSite);
+function reroute() {
+    var site = window.location;
+    if (site.search.length > 1) {
+        var newSite = site.origin;
+        newSite += site.search.substring(1);
+        site.replace(newSite);
+    }
 }
