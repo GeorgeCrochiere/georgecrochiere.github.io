4f9bf2c6b21e0a88aaafda0e9cf5896a4dd4fbc6
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2023-08-23 21:34:13 -0400

    Changed Redirect to Blazor Navigation

--- src/js/redirect.js
+++
@@ -1,8 +0,0 @@
-﻿function reroute() {
-    var site = window.location;
-    if (site.search.length > 1) {
-        var siteExtension = site.search.substring(1);
-        return siteExtension;
-    }
-    return "/";
-}
